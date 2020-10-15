2ad8a71539484b223086fd75b702eec8f8755e48
Joris Janßen <33102957+j0risj@users.noreply.github.com>
2020-10-15 17:57:48 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="discordlogger-j0risj", # Replace with your own username
-    version="0.0.1",
+    name="discordlogger-j0risj",
+    version="0.0.2",
     author="j0risj",
     author_email="j0risj@outlook.de",
     description="A logging handler for Discord",
@@ -13,10 +13,14 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/j0risj/discordlogger",
     packages=setuptools.find_packages(),
+    license="LICENSE.md",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
+    install_requires=[
+        "requests"
+    ]
 )
