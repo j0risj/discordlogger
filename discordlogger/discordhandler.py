cf4ae00b8142f7a05f34d141ea90781902aad8c1
j0risj <joma12912@gmail.com>
2020-10-15 13:53:42 +0200

    Add some documentation

--- discordlogger/discordhandler.py
+++ discordlogger/discordhandler.py
@@ -3,9 +3,20 @@ import logging
 
 
 class DiscordHandler(logging.Handler):
+    """Handler for logging to Discord webhook
+    """
     def __init__(self, username: str,
                  webhook_url: str,
                  avatar_url: str = None):
+        """Initialize the logging handler
+
+        Args:
+            username (str): Discord username the log message comes from
+            webhook_url (str): Full Discord webhook url
+            avatar_url (str, optional): Avatar picture url. 
+            See Discord webhook documentation for more details. 
+            Defaults to None.
+        """
         logging.Handler.__init__(self)
         self.json_data = {
             "username": username}
@@ -18,3 +29,4 @@ class DiscordHandler(logging.Handler):
         self.json_data["content"] = f"```\n{msg}\n```"
         response = requests.post(self.webhook_url, json=self.json_data)
         response.raise_for_status()
+
